1dab5ec39734c4e57c653e94b584f1eef9f3be63
Daniel Neer <daniel.r.neer@gmail.com>
2022-09-19 16:39:35 -0700

    Add formatting

--- cypress/e2e/flight-details-spec.cy.js
+++ cypress/e2e/flight-details-spec.cy.js
@@ -1,37 +1,65 @@
-describe('Flight details user flow', () => {
-  Cypress.on('uncaught:exception', (err, runnable) => {
+describe("Flight details user flow", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
     return false;
   });
-  
+
   beforeEach(() => {
-    cy.visit('https://www.flybreeze.com/booking/availability?origin=PVU&destination=CHS&beginDate=2022-11-17&endDate=2022-11-25&searchDestinationMacs=false&searchOriginMacs=false&passengers=%7B%22types%22:%5B%7B%22count%22:2,%22type%22:%22ADT%22%7D%5D%7D&infantCount=0')
-    cy.wait(3000)
-  })
+    cy.visit(
+      "https://www.flybreeze.com/booking/availability?origin=PVU&destination=CHS&beginDate=2022-11-17&endDate=2022-11-25&searchDestinationMacs=false&searchOriginMacs=false&passengers=%7B%22types%22:%5B%7B%22count%22:2,%22type%22:%22ADT%22%7D%5D%7D&infantCount=0"
+    );
+    cy.wait(3000);
+  });
 
-  it('Should test that outbound selected details are correct', () => {
-    cy.get(':nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > #ti-flight-details-date > :nth-child(2)').should('have.text', 'Nov 17, 2022')
-    cy.get(':nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > :nth-child(1) > .station-name').should('have.text', ' Provo Municipal Airport (PVU) ')
-    cy.get(':nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > .text-right > .station-name').should('have.text', ' Charleston International Airport (CHS) ')
-    cy.get(':nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price').should('be.visible')
-    cy.get(':nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price').should('be.visible')
-  })
+  it("Should test that outbound selected details are correct", () => {
+    cy.get(
+      ":nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > #ti-flight-details-date > :nth-child(2)"
+    ).should("have.text", "Nov 17, 2022");
+    cy.get(
+      ":nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > :nth-child(1) > .station-name"
+    ).should("have.text", " Provo Municipal Airport (PVU) ");
+    cy.get(
+      ":nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > .text-right > .station-name"
+    ).should("have.text", " Charleston International Airport (CHS) ");
+    cy.get(
+      ":nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price"
+    ).should("be.visible");
+    cy.get(
+      ":nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price"
+    ).should("be.visible");
+  });
 
-  it('Should test that inbound selected details are correct', () => {
-    cy.get(':nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > #ti-flight-details-date > :nth-child(2)').should('have.text', 'Nov 25, 2022')
-    cy.get(':nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > :nth-child(1) > .station-name').should('have.text', ' Charleston International Airport (CHS) ')
-    cy.get(':nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > .text-right > .station-name').should('have.text', ' Provo Municipal Airport (PVU) ')
-    cy.get(':nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price').should('be.visible')
-    cy.get('#inboundFlightList > :nth-child(2) > .pb-md-4 > app-available-journey > :nth-child(1) > .flex-grow-1 > .h-100 > .align-items-center > .flight-number').should('be.visible')
-  })
+  it("Should test that inbound selected details are correct", () => {
+    cy.get(
+      ":nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > #ti-flight-details-date > :nth-child(2)"
+    ).should("have.text", "Nov 25, 2022");
+    cy.get(
+      ":nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > :nth-child(1) > .station-name"
+    ).should("have.text", " Charleston International Airport (CHS) ");
+    cy.get(
+      ":nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .selected-flight-banner-org-dst > .text-right > .station-name"
+    ).should("have.text", " Provo Municipal Airport (PVU) ");
+    cy.get(
+      ":nth-child(2) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price"
+    ).should("be.visible");
+    cy.get(
+      "#inboundFlightList > :nth-child(2) > .pb-md-4 > app-available-journey > :nth-child(1) > .flex-grow-1 > .h-100 > .align-items-center > .flight-number"
+    ).should("be.visible");
+  });
 
-  it('Should test overview and final price', () => {
-    cy.get('.mr-1 > :nth-child(1) > .font-lg').should('have.text', 'Overview')
-    cy.get('#ti-overview-guest-count').should('have.text', ' 2 GUESTS ')
-  })
+  it("Should test overview and final price", () => {
+    cy.get(".mr-1 > :nth-child(1) > .font-lg").should("have.text", "Overview");
+    cy.get("#ti-overview-guest-count").should("have.text", " 2 GUESTS ");
+  });
 
-  it('Should test selecting a different flight level', () => {
-    cy.get(':nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price').should('be.visible')
-    cy.get('#outbound-journey-0-BZU > .fare-family-card > .content > .fare-family-card-content > .fare-family-card-btn').click()
-    cy.get(':nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price').should('not.contain', ' $  134  .00 ')
-  })
-})
+  it("Should test selecting a different flight level", () => {
+    cy.get(
+      ":nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price"
+    ).should("be.visible");
+    cy.get(
+      "#outbound-journey-0-BZU > .fare-family-card > .content > .fare-family-card-content > .fare-family-card-btn"
+    ).click();
+    cy.get(
+      ":nth-child(1) > app-flight-details-banner > .d-md-block > :nth-child(1) > .container > :nth-child(3) > .col-10 > .flex-shrink-0 > .tc-flight-fare-details > app-price > .price"
+    ).should("not.contain", " $  134  .00 ");
+  });
+});
--- cypress/e2e/homepage_trip_selection_spec.cy.js
+++ cypress/e2e/homepage_trip_selection_spec.cy.js
@@ -1,46 +1,69 @@
-describe('Homepage selection user flow', () => {
-  Cypress.on('uncaught:exception', (err, runnable) => {
+describe("Homepage selection user flow", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
     return false;
   });
 
   beforeEach(() => {
-    cy.visit('https://www.flybreeze.com/home')
-  })
+    cy.visit("https://www.flybreeze.com/home");
+  });
 
-  it('Should test user flow for selecting a flight', () => {
-    cy.get('#ti-origin-select').click().type('utah')
-    cy.get('.ng-option').eq(0).click()
-    cy.get('.airport').should('have.text', ' Provo Municipal Airport (PVU) ')
-    cy.get('#ti-destination-select').click()
-    cy.get('.ng-option').eq(0).click()
-    cy.get('#ti-destination-select > .station-input > .selected-station > .airport').should('have.text', ' Charleston International Airport (CHS) ')
-    cy.get('booking-web-shared-ui-booking-passengers-card > .card > .card-body').click()
-    cy.get('[role="region"][aria-label="add adult"] > .buttons > .plus').click()
-    cy.get('.container > .pb-4').click()
-    cy.wait(6000)
-    cy.get('booking-web-shared-ui-booking-dates-card > .card > .card-body').click()
-    cy.get('[style="left:0%"] > .mbsc-cal-table > :nth-child(3) > .mbsc-cal-day4 > .mbsc-cal-cell-i > .mbsc-cal-day-date').click()
-    cy.get('[style="left:0%"] > .mbsc-cal-table > :nth-child(4) > .mbsc-cal-day5 > .mbsc-cal-cell-i > .mbsc-cal-day-date').click()
-    cy.get('#search-flights > .card > .card-body').click()
-  })
+  it("Should test user flow for selecting a flight", () => {
+    cy.get("#ti-origin-select").click().type("utah");
+    cy.get(".ng-option").eq(0).click();
+    cy.get(".airport").should("have.text", " Provo Municipal Airport (PVU) ");
+    cy.get("#ti-destination-select").click();
+    cy.get(".ng-option").eq(0).click();
+    cy.get(
+      "#ti-destination-select > .station-input > .selected-station > .airport"
+    ).should("have.text", " Charleston International Airport (CHS) ");
+    cy.get(
+      "booking-web-shared-ui-booking-passengers-card > .card > .card-body"
+    ).click();
+    cy.get(
+      '[role="region"][aria-label="add adult"] > .buttons > .plus'
+    ).click();
+    cy.get(".container > .pb-4").click();
+    cy.wait(6000);
+    cy.get(
+      "booking-web-shared-ui-booking-dates-card > .card > .card-body"
+    ).click();
+    cy.get(
+      '[style="left:0%"] > .mbsc-cal-table > :nth-child(3) > .mbsc-cal-day4 > .mbsc-cal-cell-i > .mbsc-cal-day-date'
+    ).click();
+    cy.get(
+      '[style="left:0%"] > .mbsc-cal-table > :nth-child(4) > .mbsc-cal-day5 > .mbsc-cal-cell-i > .mbsc-cal-day-date'
+    ).click();
+    cy.get("#search-flights > .card > .card-body").click();
+  });
 
-  it('Should display no stations message when user enters numbers into input', () => {
-    cy.get('#ti-origin-select').click().type('1234')
-    cy.get('.container > .font-lg').should('have.text', ' No Stations Found ')
-    cy.get('.text-light-gray').should('have.text', ` Sorry, we’re only servicing select airports for now. Check back later to see if your desired city was added. `)
-  })
+  it("Should display no stations message when user enters numbers into input", () => {
+    cy.get("#ti-origin-select").click().type("1234");
+    cy.get(".container > .font-lg").should("have.text", " No Stations Found ");
+    cy.get(".text-light-gray").should(
+      "have.text",
+      ` Sorry, we’re only servicing select airports for now. Check back later to see if your desired city was added. `
+    );
+  });
 
-  it('Should test titles are correct', () => {
-    cy.get('.text-white').should('have.text', ` It's Seriously Nice™ to see you. `)
-    cy.get('.col-12 > .text-secondary').should('have.text', ' Book your flight now through May 16, 2023. ')
-  })
+  it("Should test titles are correct", () => {
+    cy.get(".text-white").should(
+      "have.text",
+      ` It's Seriously Nice™ to see you. `
+    );
+    cy.get(".col-12 > .text-secondary").should(
+      "have.text",
+      " Book your flight now through May 16, 2023. "
+    );
+  });
 
-  it('Should test tc-promotion-banner exists', () => {
-    cy.get('a > .d-none').should('exist')
-  })
+  it("Should test tc-promotion-banner exists", () => {
+    cy.get("a > .d-none").should("exist");
+  });
 
-  it('Should test .svg is visible and includes the correct xmlns', () => {
-    cy.get('.svg').should('be.visible')
-    cy.get('.svg').should('have.attr', 'xmlns').should('include', 'http://www.w3.org/2000/svg')
-  })
-})
+  it("Should test .svg is visible and includes the correct xmlns", () => {
+    cy.get(".svg").should("be.visible");
+    cy.get(".svg")
+      .should("have.attr", "xmlns")
+      .should("include", "http://www.w3.org/2000/svg");
+  });
+});
